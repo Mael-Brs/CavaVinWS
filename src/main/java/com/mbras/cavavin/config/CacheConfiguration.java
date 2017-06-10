493006bb0939b53fc943c072e72b9f6846864979
Mael-Brs <mael.b_53@hotmail.fr>
2017-06-10 16:01:59 +0200

    Back Epingagle de vin

--- src/main/java/com/mbras/cavavin/config/CacheConfiguration.java
+++ src/main/java/com/mbras/cavavin/config/CacheConfiguration.java
@@ -48,6 +48,7 @@ public class CacheConfiguration {
             cm.createCache(com.mbras.cavavin.domain.WineInCellar.class.getName(), jcacheConfiguration);
             cm.createCache(com.mbras.cavavin.domain.Year.class.getName(), jcacheConfiguration);
             cm.createCache(com.mbras.cavavin.domain.WineAgingData.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mbras.cavavin.domain.PinnedVintage.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
