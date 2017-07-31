1fa8fd1bf1f281f00af4950241862e5932feb2c5
BRAS Mael <mael.bras@soprasteria.com>
2017-07-31 09:12:09 +0200

    Remove dto for pinnedVintage

--- src/main/java/com/mbras/cavavin/repository/PinnedVintageRepository.java
+++ src/main/java/com/mbras/cavavin/repository/PinnedVintageRepository.java
@@ -4,7 +4,9 @@ import com.mbras.cavavin.domain.PinnedVintage;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
 import java.util.List;
+
 
 /**
  * Spring Data JPA repository for the PinnedVintage entity.
@@ -12,8 +14,5 @@ import java.util.List;
 @SuppressWarnings("unused")
 @Repository
 public interface PinnedVintageRepository extends JpaRepository<PinnedVintage,Long> {
-
-    @Query("select pinned_vintage from PinnedVintage pinned_vintage where pinned_vintage.user.login = ?#{principal.username}")
-    List<PinnedVintage> findByUserIsCurrentUser();
-    
+    List<PinnedVintage> findByUserId(Long id);
 }
