738a400d4dcd947b083e249872bcb05221267787
BRAS Mael <mael.bras@soprasteria.com>
2017-08-05 23:07:39 +0200

    Update WineAgingData constraints

--- src/main/java/com/mbras/cavavin/repository/WineAgingDataRepository.java
+++ src/main/java/com/mbras/cavavin/repository/WineAgingDataRepository.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the WineAgingData entity.
