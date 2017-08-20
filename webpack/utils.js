9ce6d7905abf0f05fe5375d4c06f569a09b2ff0a
BRAS Mael <mael.bras@soprasteria.com>
2017-08-20 21:30:52 +0200

    suite mise à jour client

--- webpack/utils.js
+++ webpack/utils.js
@@ -7,10 +7,23 @@ module.exports = {
     isExternalLib
 };
 
-// Returns a static version number when server is skipped
+const parseString = require('xml2js').parseString;
+// return the version number from `pom.xml` file
 function parseVersion() {
-    return '0.0.1-SNAPSHOT';
-};
+    let version = null;
+    const pomXml = fs.readFileSync('pom.xml', 'utf8');
+    parseString(pomXml, (err, result) => {
+        if (result.project.version && result.project.version[0]) {
+            version = result.project.version[0];
+        } else if (result.project.parent && result.project.parent[0] && result.project.parent[0].version && result.project.parent[0].version[0]) {
+            version = result.project.parent[0].version[0];
+        }
+    });
+    if (version === null) {
+        throw new Error('pom.xml is malformed. No version is defined');
+    }
+    return version;
+}
 
 const _root = path.resolve(__dirname, '..');
 
