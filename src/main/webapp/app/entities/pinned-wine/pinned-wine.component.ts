05ce8b037b9b56f15562da663485ae831f0e26ef
BRAS Mael <mael.bras@soprasteria.com>
2017-10-01 11:29:29 +0200

    Switch Pinned vintage to pinned wine

--- src/main/webapp/app/entities/pinned-wine/pinned-wine.component.ts
+++ src/main/webapp/app/entities/pinned-wine/pinned-wine.component.ts
@@ -3,23 +3,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { JhiEventManager, JhiParseLinks, JhiPaginationUtil, JhiLanguageService, JhiAlertService } from 'ng-jhipster';
 
-import { PinnedVintage } from './pinned-vintage.model';
-import { PinnedVintageService } from './pinned-vintage.service';
+import { PinnedWine } from './pinned-wine.model';
+import { PinnedWineService } from './pinned-wine.service';
 import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
 import { PaginationConfig } from '../../blocks/config/uib-pagination.config';
 
 @Component({
-    selector: 'jhi-pinned-vintage',
-    templateUrl: './pinned-vintage.component.html'
+    selector: 'jhi-pinned-wine',
+    templateUrl: './pinned-wine.component.html'
 })
-export class PinnedVintageComponent implements OnInit, OnDestroy {
-pinnedVintages: PinnedVintage[];
+export class PinnedWineComponent implements OnInit, OnDestroy {
+pinnedWines: PinnedWine[];
     currentAccount: any;
     eventSubscriber: Subscription;
     currentSearch: string;
 
     constructor(
-        private pinnedVintageService: PinnedVintageService,
+        private pinnedWineService: PinnedWineService,
         private alertService: JhiAlertService,
         private eventManager: JhiEventManager,
         private activatedRoute: ActivatedRoute,
@@ -30,17 +30,17 @@ pinnedVintages: PinnedVintage[];
 
     loadAll() {
         if (this.currentSearch) {
-            this.pinnedVintageService.search({
+            this.pinnedWineService.search({
                 query: this.currentSearch,
                 }).subscribe(
-                    (res: ResponseWrapper) => this.pinnedVintages = res.json,
+                    (res: ResponseWrapper) => this.pinnedWines = res.json,
                     (res: ResponseWrapper) => this.onError(res.json)
                 );
             return;
        }
-        this.pinnedVintageService.query().subscribe(
+        this.pinnedWineService.query().subscribe(
             (res: ResponseWrapper) => {
-                this.pinnedVintages = res.json;
+                this.pinnedWines = res.json;
                 this.currentSearch = '';
             },
             (res: ResponseWrapper) => this.onError(res.json)
@@ -64,18 +64,18 @@ pinnedVintages: PinnedVintage[];
         this.principal.identity().then((account) => {
             this.currentAccount = account;
         });
-        this.registerChangeInPinnedVintages();
+        this.registerChangeInPinnedWines();
     }
 
     ngOnDestroy() {
         this.eventManager.destroy(this.eventSubscriber);
     }
 
-    trackId(index: number, item: PinnedVintage) {
+    trackId(index: number, item: PinnedWine) {
         return item.id;
     }
-    registerChangeInPinnedVintages() {
-        this.eventSubscriber = this.eventManager.subscribe('pinnedVintageListModification', (response) => this.loadAll());
+    registerChangeInPinnedWines() {
+        this.eventSubscriber = this.eventManager.subscribe('pinnedWineListModification', (response) => this.loadAll());
     }
 
     private onError(error) {
