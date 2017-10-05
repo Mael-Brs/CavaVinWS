6e4ce56f8f008580def78d6d85304e2f9aab7d30
mbras <mael.b_53@hotmail.fr>
2017-10-05 19:13:08 +0200

    Finish deleting searengine for region

--- src/main/webapp/app/entities/region/region.service.ts
+++ src/main/webapp/app/entities/region/region.service.ts
@@ -9,7 +9,6 @@ import { ResponseWrapper, createRequestOption } from '../../shared';
 export class RegionService {
 
     private resourceUrl = 'api/regions';
-    private resourceSearchUrl = 'api/_search/regions';
 
     constructor(private http: Http) { }
 
@@ -41,12 +40,6 @@ export class RegionService {
 
     delete(id: number): Observable<Response> {
         return this.http.delete(`${this.resourceUrl}/${id}`);
-    }
-
-    search(req?: any): Observable<ResponseWrapper> {
-        const options = createRequestOption(req);
-        return this.http.get(this.resourceSearchUrl, options)
-            .map((res: any) => this.convertResponse(res));
     }
 
     private convertResponse(res: Response): ResponseWrapper {
