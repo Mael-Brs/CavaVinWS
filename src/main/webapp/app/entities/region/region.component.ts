dcf44e23ecd62bea86640cca52c39a9aaae45162
mbras <mael.b_53@hotmail.fr>
2017-10-05 19:13:08 +0200

    Finish deleting searengine for region

--- src/main/webapp/app/entities/region/region.component.ts
+++ src/main/webapp/app/entities/region/region.component.ts
@@ -16,48 +16,22 @@ export class RegionComponent implements OnInit, OnDestroy {
 regions: Region[];
     currentAccount: any;
     eventSubscriber: Subscription;
-    currentSearch: string;
 
     constructor(
         private regionService: RegionService,
         private alertService: JhiAlertService,
         private eventManager: JhiEventManager,
-        private activatedRoute: ActivatedRoute,
         private principal: Principal
     ) {
-        this.currentSearch = activatedRoute.snapshot.params['search'] ? activatedRoute.snapshot.params['search'] : '';
     }
 
     loadAll() {
-        if (this.currentSearch) {
-            this.regionService.search({
-                query: this.currentSearch,
-                }).subscribe(
-                    (res: ResponseWrapper) => this.regions = res.json,
-                    (res: ResponseWrapper) => this.onError(res.json)
-                );
-            return;
-       }
         this.regionService.query().subscribe(
             (res: ResponseWrapper) => {
                 this.regions = res.json;
-                this.currentSearch = '';
             },
             (res: ResponseWrapper) => this.onError(res.json)
         );
-    }
-
-    search(query) {
-        if (!query) {
-            return this.clear();
-        }
-        this.currentSearch = query;
-        this.loadAll();
-    }
-
-    clear() {
-        this.currentSearch = '';
-        this.loadAll();
     }
     ngOnInit() {
         this.loadAll();
