c2d851a182804e1fb30b0d9c3c617c8239ed620d
mbras <mael.b_53@hotmail.fr>
2017-10-05 22:26:58 +0200

    Finish deleting search engine for color

--- src/main/webapp/app/entities/color/color.service.ts
+++ src/main/webapp/app/entities/color/color.service.ts
@@ -9,7 +9,6 @@ import { ResponseWrapper, createRequestOption } from '../../shared';
 export class ColorService {
 
     private resourceUrl = 'api/colors';
-    private resourceSearchUrl = 'api/_search/colors';
 
     constructor(private http: Http) { }
 
@@ -41,12 +40,6 @@ export class ColorService {
 
     delete(id: number): Observable<Response> {
         return this.http.delete(`${this.resourceUrl}/${id}`);
-    }
-
-    search(req?: any): Observable<ResponseWrapper> {
-        const options = createRequestOption(req);
-        return this.http.get(this.resourceSearchUrl, options)
-            .map((res: any) => this.convertResponse(res));
     }
 
     private convertResponse(res: Response): ResponseWrapper {
