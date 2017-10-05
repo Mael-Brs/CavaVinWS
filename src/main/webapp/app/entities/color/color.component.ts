f72d211c56ba2779ef167aa756881d6b56a9a4ca
mbras <mael.b_53@hotmail.fr>
2017-10-05 22:26:58 +0200

    Finish deleting search engine for color

--- src/main/webapp/app/entities/color/color.component.ts
+++ src/main/webapp/app/entities/color/color.component.ts
@@ -16,48 +16,22 @@ export class ColorComponent implements OnInit, OnDestroy {
 colors: Color[];
     currentAccount: any;
     eventSubscriber: Subscription;
-    currentSearch: string;
 
     constructor(
         private colorService: ColorService,
         private alertService: JhiAlertService,
         private eventManager: JhiEventManager,
-        private activatedRoute: ActivatedRoute,
         private principal: Principal
     ) {
-        this.currentSearch = activatedRoute.snapshot.params['search'] ? activatedRoute.snapshot.params['search'] : '';
     }
 
     loadAll() {
-        if (this.currentSearch) {
-            this.colorService.search({
-                query: this.currentSearch,
-                }).subscribe(
-                    (res: ResponseWrapper) => this.colors = res.json,
-                    (res: ResponseWrapper) => this.onError(res.json)
-                );
-            return;
-       }
         this.colorService.query().subscribe(
             (res: ResponseWrapper) => {
                 this.colors = res.json;
-                this.currentSearch = '';
             },
             (res: ResponseWrapper) => this.onError(res.json)
         );
-    }
-
-    search(query) {
-        if (!query) {
-            return this.clear();
-        }
-        this.currentSearch = query;
-        this.loadAll();
-    }
-
-    clear() {
-        this.currentSearch = '';
-        this.loadAll();
     }
     ngOnInit() {
         this.loadAll();
