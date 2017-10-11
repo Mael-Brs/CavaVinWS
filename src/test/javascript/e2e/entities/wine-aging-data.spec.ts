f252ba5328f5be25c6bba9725c6a6e066097ce59
BRAS Mael <mael.bras@soprasteria.com>
2017-10-11 21:26:54 +0200

    Upgrade WineAgingData entity

--- src/test/javascript/e2e/entities/wine-aging-data.spec.ts
+++ src/test/javascript/e2e/entities/wine-aging-data.spec.ts
@@ -1,49 +1,138 @@
 import { browser, element, by, $ } from 'protractor';
+import { NavBarPage } from './../page-objects/jhi-page-objects';
+const path = require('path');
 
 describe('WineAgingData e2e test', () => {
 
-    const username = element(by.id('username'));
-    const password = element(by.id('password'));
-    const entityMenu = element(by.id('entity-menu'));
-    const accountMenu = element(by.id('account-menu'));
-    const login = element(by.id('login'));
-    const logout = element(by.id('logout'));
+    let navBarPage: NavBarPage;
+    let wineAgingDataDialogPage: WineAgingDataDialogPage;
+    let wineAgingDataComponentsPage: WineAgingDataComponentsPage;
+    const fileToUpload = '../../../../main/webapp/content/images/logo-jhipster.png';
+    const absolutePath = path.resolve(__dirname, fileToUpload);
+    
 
     beforeAll(() => {
         browser.get('/');
-
-        accountMenu.click();
-        login.click();
-
-        username.sendKeys('admin');
-        password.sendKeys('admin');
-        element(by.css('button[type=submit]')).click();
+        browser.waitForAngular();
+        navBarPage = new NavBarPage();
+        navBarPage.getSignInPage().autoSignInUsing('admin', 'admin');
         browser.waitForAngular();
     });
 
     it('should load WineAgingData', () => {
-        entityMenu.click();
-        element.all(by.css('[routerLink="wine-aging-data"]')).first().click().then(() => {
-            const expectVal = /cavavinApp.wineAgingData.home.title/;
-            element.all(by.css('h2 span')).first().getAttribute('jhiTranslate').then((value) => {
-                expect(value).toMatch(expectVal);
-            });
-        });
+        navBarPage.goToEntity('wine-aging-data');
+        wineAgingDataComponentsPage = new WineAgingDataComponentsPage();
+        expect(wineAgingDataComponentsPage.getTitle()).toMatch(/cavavinApp.wineAgingData.home.title/);
+
     });
 
     it('should load create WineAgingData dialog', () => {
-        element(by.css('button.create-wine-aging-data')).click().then(() => {
-            const expectVal = /cavavinApp.wineAgingData.home.createOrEditLabel/;
-            element.all(by.css('h4.modal-title')).first().getAttribute('jhiTranslate').then((value) => {
-                expect(value).toMatch(expectVal);
-            });
-
-            element(by.css('button.close')).click();
-        });
+        wineAgingDataComponentsPage.clickOnCreateButton();
+        wineAgingDataDialogPage = new WineAgingDataDialogPage();
+        expect(wineAgingDataDialogPage.getModalTitle()).toMatch(/cavavinApp.wineAgingData.home.createOrEditLabel/);
+        wineAgingDataDialogPage.close();
     });
+
+   /* it('should create and save WineAgingData', () => {
+        wineAgingDataComponentsPage.clickOnCreateButton();
+        wineAgingDataDialogPage.setMinKeepInput('5');
+        expect(wineAgingDataDialogPage.getMinKeepInput()).toMatch('5');
+        wineAgingDataDialogPage.setMaxKeepInput('5');
+        expect(wineAgingDataDialogPage.getMaxKeepInput()).toMatch('5');
+        wineAgingDataDialogPage.colorSelectLastOption();
+        wineAgingDataDialogPage.regionSelectLastOption();
+        wineAgingDataDialogPage.save();
+        expect(wineAgingDataDialogPage.getSaveButton().isPresent()).toBeFalsy();
+    }); */
 
     afterAll(() => {
-        accountMenu.click();
-        logout.click();
+        navBarPage.autoSignOut();
     });
 });
+
+export class WineAgingDataComponentsPage {
+    createButton = element(by.css('.jh-create-entity'));
+    title = element.all(by.css('jhi-wine-aging-data div h2 span')).first();
+
+    clickOnCreateButton() {
+        return this.createButton.click();
+    }
+
+    getTitle() {
+        return this.title.getAttribute('jhiTranslate');
+    }
+}
+
+export class WineAgingDataDialogPage {
+    modalTitle = element(by.css('h4#myWineAgingDataLabel'));
+    saveButton = element(by.css('.modal-footer .btn.btn-primary'));
+    closeButton = element(by.css('button.close'));
+    minKeepInput = element(by.css('input#field_minKeep'));
+    maxKeepInput = element(by.css('input#field_maxKeep'));
+    colorSelect = element(by.css('select#field_color'));
+    regionSelect = element(by.css('select#field_region'));
+
+    getModalTitle() {
+        return this.modalTitle.getAttribute('jhiTranslate');
+    }
+
+    setMinKeepInput = function (minKeep) {
+        this.minKeepInput.sendKeys(minKeep);
+    }
+
+    getMinKeepInput = function () {
+        return this.minKeepInput.getAttribute('value');
+    }
+
+    setMaxKeepInput = function (maxKeep) {
+        this.maxKeepInput.sendKeys(maxKeep);
+    }
+
+    getMaxKeepInput = function () {
+        return this.maxKeepInput.getAttribute('value');
+    }
+
+    colorSelectLastOption = function () {
+        this.colorSelect.all(by.tagName('option')).last().click();
+    }
+
+    colorSelectOption = function (option) {
+        this.colorSelect.sendKeys(option);
+    }
+
+    getColorSelect = function () {
+        return this.colorSelect;
+    }
+
+    getColorSelectedOption = function () {
+        return this.colorSelect.element(by.css('option:checked')).getText();
+    }
+
+    regionSelectLastOption = function () {
+        this.regionSelect.all(by.tagName('option')).last().click();
+    }
+
+    regionSelectOption = function (option) {
+        this.regionSelect.sendKeys(option);
+    }
+
+    getRegionSelect = function () {
+        return this.regionSelect;
+    }
+
+    getRegionSelectedOption = function () {
+        return this.regionSelect.element(by.css('option:checked')).getText();
+    }
+
+    save() {
+        this.saveButton.click();
+    }
+
+    close() {
+        this.closeButton.click();
+    }
+
+    getSaveButton() {
+        return this.saveButton;
+    }
+}
