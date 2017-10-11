0e71ac09cd9d1e9a776ec3f1c54239020535faeb
Mael-Brs <mael.b_53@hotmail.fr>
2017-10-11 18:56:11 +0200

    Upgrade PinnedWine entity

--- src/main/webapp/app/entities/pinned-wine/pinned-wine-detail.component.ts
+++ src/main/webapp/app/entities/pinned-wine/pinned-wine-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
-import { JhiEventManager  } from 'ng-jhipster';
+import { JhiEventManager } from 'ng-jhipster';
 
 import { PinnedWine } from './pinned-wine.model';
 import { PinnedWineService } from './pinned-wine.service';
