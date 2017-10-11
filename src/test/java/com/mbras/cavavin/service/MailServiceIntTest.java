a1aff8fef1ccf518e931a0698f18d4151da02575
Mael-Brs <mael.b_53@hotmail.fr>
2017-10-11 18:10:09 +0200

    Upgrade to jhipster 4.9.0

--- src/test/java/com/mbras/cavavin/service/MailServiceIntTest.java
+++ src/test/java/com/mbras/cavavin/service/MailServiceIntTest.java
@@ -1,4 +1,5 @@
 package com.mbras.cavavin.service;
+import com.mbras.cavavin.config.Constants;
 
 import com.mbras.cavavin.CavavinApp;
 import com.mbras.cavavin.domain.User;
@@ -135,7 +136,7 @@ public class MailServiceIntTest {
     @Test
     public void testSendActivationEmail() throws Exception {
         User user = new User();
-        user.setLangKey("fr");
+        user.setLangKey(Constants.DEFAULT_LANGUAGE);
         user.setLogin("john");
         user.setEmail("john.doe@example.com");
         mailService.sendActivationEmail(user);
@@ -150,7 +151,7 @@ public class MailServiceIntTest {
     @Test
     public void testCreationEmail() throws Exception {
         User user = new User();
-        user.setLangKey("fr");
+        user.setLangKey(Constants.DEFAULT_LANGUAGE);
         user.setLogin("john");
         user.setEmail("john.doe@example.com");
         mailService.sendCreationEmail(user);
@@ -165,7 +166,7 @@ public class MailServiceIntTest {
     @Test
     public void testSendPasswordResetMail() throws Exception {
         User user = new User();
-        user.setLangKey("fr");
+        user.setLangKey(Constants.DEFAULT_LANGUAGE);
         user.setLogin("john");
         user.setEmail("john.doe@example.com");
         mailService.sendPasswordResetMail(user);
