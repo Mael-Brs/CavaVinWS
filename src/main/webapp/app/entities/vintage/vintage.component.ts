bd9dd0f75a7a600466a4c6679d7664ad5af869b4
BRAS Mael <mael.bras@soprasteria.com>
2017-10-11 21:38:58 +0200

    Upgrade Vintage entity

--- src/main/webapp/app/entities/vintage/vintage.component.ts
+++ src/main/webapp/app/entities/vintage/vintage.component.ts
@@ -19,7 +19,7 @@ vintages: Vintage[];
 
     constructor(
         private vintageService: VintageService,
-        private alertService: JhiAlertService,
+        private jhiAlertService: JhiAlertService,
         private eventManager: JhiEventManager,
         private principal: Principal
     ) {
@@ -53,6 +53,6 @@ vintages: Vintage[];
     }
 
     private onError(error) {
-        this.alertService.error(error.message, null, null);
+        this.jhiAlertService.error(error.message, null, null);
     }
 }
