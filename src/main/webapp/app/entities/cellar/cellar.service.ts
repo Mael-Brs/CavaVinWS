e92acb82d06d9fc14da0c991faa64ec6cf1cf472
BRAS Mael <mael.bras@soprasteria.com>
2017-10-11 21:53:00 +0200

    Upgrade cellar entity

--- src/main/webapp/app/entities/cellar/cellar.service.ts
+++ src/main/webapp/app/entities/cellar/cellar.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
+import { SERVER_API_URL } from '../../app.constants';
 
 import { Cellar } from './cellar.model';
 import { ResponseWrapper, createRequestOption } from '../../shared';
@@ -8,27 +9,30 @@ import { ResponseWrapper, createRequestOption } from '../../shared';
 @Injectable()
 export class CellarService {
 
-    private resourceUrl = 'api/cellars';
+    private resourceUrl = SERVER_API_URL + 'api/cellars';
 
     constructor(private http: Http) { }
 
     create(cellar: Cellar): Observable<Cellar> {
         const copy = this.convert(cellar);
         return this.http.post(this.resourceUrl, copy).map((res: Response) => {
-            return res.json();
+            const jsonResponse = res.json();
+            return this.convertItemFromServer(jsonResponse);
         });
     }
 
     update(cellar: Cellar): Observable<Cellar> {
         const copy = this.convert(cellar);
         return this.http.put(this.resourceUrl, copy).map((res: Response) => {
-            return res.json();
+            const jsonResponse = res.json();
+            return this.convertItemFromServer(jsonResponse);
         });
     }
 
     find(id: number): Observable<Cellar> {
         return this.http.get(`${this.resourceUrl}/${id}`).map((res: Response) => {
-            return res.json();
+            const jsonResponse = res.json();
+            return this.convertItemFromServer(jsonResponse);
         });
     }
 
@@ -44,9 +48,24 @@ export class CellarService {
 
     private convertResponse(res: Response): ResponseWrapper {
         const jsonResponse = res.json();
-        return new ResponseWrapper(res.headers, jsonResponse, res.status);
+        const result = [];
+        for (let i = 0; i < jsonResponse.length; i++) {
+            result.push(this.convertItemFromServer(jsonResponse[i]));
+        }
+        return new ResponseWrapper(res.headers, result, res.status);
     }
 
+    /**
+     * Convert a returned JSON object to Cellar.
+     */
+    private convertItemFromServer(json: any): Cellar {
+        const entity: Cellar = Object.assign(new Cellar(), json);
+        return entity;
+    }
+
+    /**
+     * Convert a Cellar to a JSON which can be sent to the server.
+     */
     private convert(cellar: Cellar): Cellar {
         const copy: Cellar = Object.assign({}, cellar);
         return copy;
