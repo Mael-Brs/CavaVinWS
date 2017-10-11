a20de2d71b602787f7a302c6efd3537806f10490
BRAS Mael <mael.bras@soprasteria.com>
2017-10-11 21:26:54 +0200

    Upgrade WineAgingData entity

--- src/main/webapp/app/entities/wine-aging-data/wine-aging-data-dialog.component.ts
+++ src/main/webapp/app/entities/wine-aging-data/wine-aging-data-dialog.component.ts
@@ -28,7 +28,7 @@ export class WineAgingDataDialogComponent implements OnInit {
 
     constructor(
         public activeModal: NgbActiveModal,
-        private alertService: JhiAlertService,
+        private jhiAlertService: JhiAlertService,
         private wineAgingDataService: WineAgingDataService,
         private colorService: ColorService,
         private regionService: RegionService,
@@ -61,7 +61,7 @@ export class WineAgingDataDialogComponent implements OnInit {
 
     private subscribeToSaveResponse(result: Observable<WineAgingData>) {
         result.subscribe((res: WineAgingData) =>
-            this.onSaveSuccess(res), (res: Response) => this.onSaveError(res));
+            this.onSaveSuccess(res), (res: Response) => this.onSaveError());
     }
 
     private onSaveSuccess(result: WineAgingData) {
@@ -70,18 +70,12 @@ export class WineAgingDataDialogComponent implements OnInit {
         this.activeModal.dismiss(result);
     }
 
-    private onSaveError(error) {
-        try {
-            error.json();
-        } catch (exception) {
-            error.message = error.text();
-        }
+    private onSaveError() {
         this.isSaving = false;
-        this.onError(error);
     }
 
-    private onError(error) {
-        this.alertService.error(error.message, null, null);
+    private onError(error: any) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 
     trackColorById(index: number, item: Color) {
