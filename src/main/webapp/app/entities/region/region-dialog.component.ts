3a5baee3846822d1bae471d723307fde03739f1b
Mael-Brs <mael.b_53@hotmail.fr>
2017-10-11 18:21:00 +0200

    Upgrade Region entity

--- src/main/webapp/app/entities/region/region-dialog.component.ts
+++ src/main/webapp/app/entities/region/region-dialog.component.ts
@@ -21,7 +21,7 @@ export class RegionDialogComponent implements OnInit {
 
     constructor(
         public activeModal: NgbActiveModal,
-        private alertService: JhiAlertService,
+        private jhiAlertService: JhiAlertService,
         private regionService: RegionService,
         private eventManager: JhiEventManager
     ) {
@@ -48,7 +48,7 @@ export class RegionDialogComponent implements OnInit {
 
     private subscribeToSaveResponse(result: Observable<Region>) {
         result.subscribe((res: Region) =>
-            this.onSaveSuccess(res), (res: Response) => this.onSaveError(res));
+            this.onSaveSuccess(res), (res: Response) => this.onSaveError());
     }
 
     private onSaveSuccess(result: Region) {
@@ -57,18 +57,12 @@ export class RegionDialogComponent implements OnInit {
         this.activeModal.dismiss(result);
     }
 
-    private onSaveError(error) {
-        try {
-            error.json();
-        } catch (exception) {
-            error.message = error.text();
-        }
+    private onSaveError() {
         this.isSaving = false;
-        this.onError(error);
     }
 
-    private onError(error) {
-        this.alertService.error(error.message, null, null);
+    private onError(error: any) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 }
 
