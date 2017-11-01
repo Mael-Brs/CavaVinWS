508d387695683595efb47efdf86b836da42b7952
BRAS Mael <mael.bras@soprasteria.com>
2017-11-01 19:02:00 +0100

    Add cache for WineAgingData select by region and color

--- src/main/java/com/mbras/cavavin/repository/WineAgingDataRepository.java
+++ src/main/java/com/mbras/cavavin/repository/WineAgingDataRepository.java
@@ -3,9 +3,9 @@ package com.mbras.cavavin.repository;
 import com.mbras.cavavin.domain.Color;
 import com.mbras.cavavin.domain.Region;
 import com.mbras.cavavin.domain.WineAgingData;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import org.springframework.data.jpa.repository.*;
 
 
 /**
@@ -14,5 +14,6 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface WineAgingDataRepository extends JpaRepository<WineAgingData,Long> {
+    @Cacheable("AgingDataByColorAndRegion")
     WineAgingData findByColorAndRegion(Color color, Region region);
 }
