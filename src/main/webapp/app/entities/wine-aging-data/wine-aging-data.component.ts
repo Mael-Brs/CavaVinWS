d3fe28c261de9f9313e61954572731782f0c18ef
Mael-Brs <mael.b_53@hotmail.fr>
2017-12-07 14:01:15 +0100

    Updating WineAgingData entity

--- src/main/webapp/app/entities/wine-aging-data/wine-aging-data.component.ts
+++ src/main/webapp/app/entities/wine-aging-data/wine-aging-data.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
-import { JhiEventManager, JhiParseLinks, JhiPaginationUtil, JhiLanguageService, JhiAlertService } from 'ng-jhipster';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
 import { WineAgingData } from './wine-aging-data.model';
 import { WineAgingDataService } from './wine-aging-data.service';
 import { ITEMS_PER_PAGE, Principal, ResponseWrapper } from '../../shared';
-import { PaginationConfig } from '../../blocks/config/uib-pagination.config';
 
 @Component({
     selector: 'jhi-wine-aging-data',
